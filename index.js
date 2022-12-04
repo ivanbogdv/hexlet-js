e5d1071f1f099c6527fb977c761cba834fad8899
ivan.bogdv@gmail.com <ivan.bogdv@gmail.com>
2022-12-04 15:34:25 +0300

    Upgrade index.js package-lock.json package.json

--- index.js
+++ index.js
@@ -2,4 +2,10 @@ console.log("Hello, Hexlet!");
 
 import _ from "lodash";
 // Пересечение массивов
-console.log(_.last(['one', 'two']));
+console.log(_.last(["one", "two"]));
+
+
+const fn = (a, b) => {
+
+    const c = a + b; return c;
+}
